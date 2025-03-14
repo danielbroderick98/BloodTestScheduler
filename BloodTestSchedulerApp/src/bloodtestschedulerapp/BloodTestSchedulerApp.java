1614ffbddea049fa0d84d4fd48f67f6eab75bede
Daniel <65319057+danielbroderick98@users.noreply.github.com>
2025-03-14 13:27:54 +0000

    Add files via upload

---
+++ BloodTestSchedulerApp/src/bloodtestschedulerapp/BloodTestSchedulerApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodtestschedulerapp;
+
+/**
+ *
+ * @author dbrod
+ */
+public class BloodTestSchedulerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Running the GUI to Display
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new BloodTestGui().setVisible(true);
+            }
+        });
+    }
+    
+}
